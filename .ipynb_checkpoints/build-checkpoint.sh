7c2353a6ba6cd47d0a4b9b6d630078e9a3936c2c
mocmjmm6 <mocmjmm6@B-LOSX7R6GF.local>
2024-10-11 11:12:40 +0100

    updates

--- .ipynb_checkpoints/build-checkpoint.sh
+++ .ipynb_checkpoints/build-checkpoint.sh
@@ -2,7 +2,7 @@
 
 rm -rf ./_build
 jupyter-book build ./
-cp misaligned.html _build/html/mislaigned.html
+cp misaligned.html _build/html/misaligned.html
 git add -A
 git commit -m 'updates'
 git push
