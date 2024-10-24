6ff7ffaf330a3ba3553612b3a29335b46670cb3a
mocmjmm6 <mocmjmm6@B-LOSX7R6GF.local>
2024-10-24 10:05:03 +0100

    updates

--- .ipynb_checkpoints/build-checkpoint.sh
+++ .ipynb_checkpoints/build-checkpoint.sh
@@ -4,6 +4,7 @@ rm -rf ./_build
 jupyter-book build ./
 cp misaligned.html _build/html/misaligned.html
 cp mni.html _build/html/mni.html
+cp deform-x.html _build/html/deform-x.html
 git add -A
 git commit -m 'updates'
 git push
--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@ rd -r .\_build
 jupyter-book build .\
 copy misaligned.html _build/html/misaligned.html
 copy mni.html _build/html/mni.html
+copy deform-x.html _build/html/deform-x.html
 git add -A
 git commit -m 'updates'
 git push
